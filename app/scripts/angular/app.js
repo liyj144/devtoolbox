a80cd09f332941f1299d02310540380a185625dc
李延杰 <liyanjie419@pingan.com.cn>
2016-05-10 18:31:08 +0800

    add php time functions

--- app/scripts/angular/app.js
+++ app/scripts/angular/app.js
@@ -1,9 +1,17 @@
 'use strict';
 
-var mainApp = angular.module('mainApp', ['ngRoute', "angularMoment", "ui.bootstrap"]);
-mainApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+var mainApp = angular.module('mainApp', [
+	'ngRoute', 
+	"angularMoment", 
+	"ui.bootstrap", 
+	"hljs"]);
+mainApp.config(function($routeProvider, $locationProvider, hljsServiceProvider) {
     $routeProvider
         .when('/', {templateUrl: '/views/tpl/welcome.html', controller: 'WelcomeCtrl'})
         .otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
-}]);
+
+    hljsServiceProvider.setOptions({
+    	tabReplace: '    '
+    });
+});
